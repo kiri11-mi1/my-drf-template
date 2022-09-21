e87fc208c8c00b577fbb5d5e9b45e07349e18e65
Kirill Mil <kirill.mil.0310@mail.ru>
2022-09-21 09:58:40 +0700

    add basename for register rout

--- src/apps/users/urls/auth.py
+++ src/apps/users/urls/auth.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from apps.users.views import UserCreateAPIView
 
 urlpatterns = [
-    path('register/', UserCreateAPIView.as_view()),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
